c3796bb3ab1d5dee1c7951ff4d6c22a52bb48de4
Tam <ilovepc830@gmail.com>
2019-03-22 10:19:33 +0700

    Create new user

--- 1-hello/index.js
+++ 1-hello/index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var port = 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+var users = [
+	{id: 1, name: 'Thinh'},
+	{id: 2, name: 'Minh'},
+	{id: 3, name: 'Tam'},
+	{id: 4, name: 'Banh'}
+];
 app.get('/', function(req, res){
 	res.render('index', {
 		name: 'AAA'
@@ -14,11 +25,32 @@ app.get('/', function(req, res){
 
 app.get('/users', function(req, res){
 	res.render('users/index', {
-		users: [
-			{id: 1, name: 'Thinh'},
-			{id: 2, name: 'Minh'}
-		]
+		users: users
 	});
+});
+
+
+app.get('/users/search', function(req, res){
+	var q = req.query.q;
+	// sessionStorage.setItem('q', q);
+
+	var matchedUsers = users.filter(function(user){
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	
+	res.render('users/index', {
+		users: matchedUsers,
+	});
+});
+
+
+app.get('/users/create', function(req, res){
+	res.render('users/create');
+});
+
+app.post('/users/create', function(req, res){
+	users.push(req.body);
+	res.redirect('/users');
 });
 
 app.listen(port, function(){
