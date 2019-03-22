ea8f7c733a4053cc8eb7a3e17d9c26e308aa6e70
Tam <ilovepc830@gmail.com>
2019-03-22 10:40:06 +0700

    lowdb: add and search user

--- 1-hello/index.js
+++ 1-hello/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+
+db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write();
 
 var port = 3000;
 
@@ -10,13 +19,6 @@ app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
-var users = [
-	{id: 1, name: 'Thinh'},
-	{id: 2, name: 'Minh'},
-	{id: 3, name: 'Tam'},
-	{id: 4, name: 'Banh'}
-];
 app.get('/', function(req, res){
 	res.render('index', {
 		name: 'AAA'
@@ -25,7 +27,7 @@ app.get('/', function(req, res){
 
 app.get('/users', function(req, res){
 	res.render('users/index', {
-		users: users
+		users: db.get('users').value()
 	});
 });
 
@@ -33,7 +35,7 @@ app.get('/users', function(req, res){
 app.get('/users/search', function(req, res){
 	var q = req.query.q;
 	// sessionStorage.setItem('q', q);
-
+	var users = db.get('users').value();
 	var matchedUsers = users.filter(function(user){
 		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
 	});
@@ -49,7 +51,7 @@ app.get('/users/create', function(req, res){
 });
 
 app.post('/users/create', function(req, res){
-	users.push(req.body);
+	db.get('users').push(req.body).write();
 	res.redirect('/users');
 });
 
